5032337822b19d1cc15643764912bf5e2ca3a7fe
nishant2003 <108606959+nishant2003@users.noreply.github.com>
2024-06-16 02:37:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import categoryRoutes from './routes/categoryRoutes.js'
 import productRoutes from './routes/productRoutes.js'
 import cors from 'cors'
 
-const path = require('path')
+import path from 'path';
 
 
 dotenv.config();
